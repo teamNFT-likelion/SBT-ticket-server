f1519349f62615a7c136fead9a688438787822fe
dhrgodms <dhrgodms123@gmail.com>
2022-11-20 19:54:10 +0900

    fix: 주석 수정

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -8,6 +8,7 @@ const {
 module.exports = {
   getByNaver: async (req, res) => {
     const code = req.query.code; // 인가코드
+<<<<<<< HEAD
 
     try {
       // 인가코드 > 엑세스토큰
@@ -31,6 +32,42 @@ module.exports = {
     } catch (err) {
       res.status(400).send({ message: err });
     }
+=======
+    const state = req.query.state;
+    const clientId = process.env.NAVER_CLIENT_ID;
+    const clientSecret = process.env.NAVER_CLIENT_SECRET;
+    // TODO: 환경별 수정필요
+    const redirectUrl =
+      state === "local"
+        ? "http://localhost:3000/payment"
+        : "http://ttot.netlify.app/payment";
+    const getTokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&code=${code}&state=${state}`;
+    const getOauthDataUrl = "https://openapi.naver.com/v1/nid/me";
+
+    // 인가코드 > 엑세스토큰
+    const {
+      data: { access_token },
+    } = await axios.get(getTokenUrl);
+
+    // 엑세스토큰 > 데이터
+    const {
+      data: { response },
+    } = await axios.get(getOauthDataUrl, {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+      },
+    });
+
+    // 엑세스토큰 만료
+    const unlinkTokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=${clientId}&client_secret=${clientSecret}&access_token=${access_token}&service_provider=NAVER`;
+    await axios.get(unlinkTokenUrl);
+
+    // 쿠키만료시간 1분
+    res.cookie("oauthData", JSON.stringify(response), {
+      expires: new Date(Date.now() + 1000 * 60),
+    });
+    res.status(302).redirect(redirectUrl);
+>>>>>>> 3cddf1c (fix: 주석 수정)
   },
 
   getByKakao: async (req, res) => {
@@ -50,7 +87,11 @@ module.exports = {
       data: { access_token },
     } = await axios.post(getTokenUrl);
 
+<<<<<<< HEAD
     // 엑세스토큰 > 데이터(이메일만 받아오면 이런 코드)
+=======
+    // 엑세스토큰 > 데이터
+>>>>>>> 3cddf1c (fix: 주석 수정)
     const { data } = await axios.get(getOauthDataUrl, {
       headers: {
         Authorization: `Bearer ${access_token}`,
@@ -59,6 +100,10 @@ module.exports = {
 
     console.log(data);
 
+<<<<<<< HEAD
+=======
+    // 엑세스토큰 만료
+>>>>>>> 3cddf1c (fix: 주석 수정)
     const unlinkTokenUrl = `https://kapi.kakao.com/v1/user/unlink`;
     await axios.get(unlinkTokenUrl, {
       headers: {

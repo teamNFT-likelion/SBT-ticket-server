9e85db7496bb07ae4e9189b64ff15eb1127d9bfc
dhrgodms <dhrgodms123@gmail.com>
2022-11-22 17:54:40 +0900

    pull하고 나서 충돌 수정

--- controllers/oauth.js
+++ controllers/oauth.js
@@ -8,7 +8,6 @@ const {
 } = require('../url.js');
 
 module.exports = {
-<<<<<<< HEAD
   getByNaver: async (req, res) => {
     const code = req.query.code; // 인가코드
 
@@ -50,7 +49,7 @@ module.exports = {
       } = await axios.post(getKakaoTokenUrl(code));
 
       // 엑세스토큰 > 데이터
-      const {h
+      const {
         data
       } = await axios.get(kakaoOauthDataUrl, {
         headers: {
@@ -60,90 +59,14 @@ module.exports = {
 
       // 토큰 만료 (이거 없어도되나? 일단 만료시킴)
       await axios.get(getKakaoUnlinkUrl, {
-=======
-	getByNaver: async (req, res) => {
-		const code = req.query.code; // 인가코드
-		const state = req.query.state;
-		const clientId = process.env.NAVER_CLIENT_ID;
-		const clientSecret = process.env.NAVER_CLIENT_SECRET;
-		// TODO: 환경별 수정필요
-		const redirectUrl = state === "local" ? "http://localhost:3000/payment" : "http://ttot.netlify.app/payment";
-		const getTokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=${clientId}&client_secret=${clientSecret}&code=${code}&state=${state}`;
-		const getOauthDataUrl = "https://openapi.naver.com/v1/nid/me";
-
-		// 인가코드 > 엑세스토큰
-		const {
-			data: { access_token },
-		} = await axios.get(getTokenUrl);
-
-		// 엑세스토큰 > 데이터
-		const {
-			data: { response },
-		} = await axios.get(getOauthDataUrl, {
-			headers: {
-				Authorization: `Bearer ${access_token}`,
-			},
-		});
-
-		// 엑세스토큰 만료
-		const unlinkTokenUrl = `https://nid.naver.com/oauth2.0/token?grant_type=delete&client_id=${clientId}&client_secret=${clientSecret}&access_token=${access_token}&service_provider=NAVER`;
-		await axios.get(unlinkTokenUrl);
-
-		// 쿠키만료시간 1분
-		res.cookie("oauthData", JSON.stringify(response), { expires: new Date(Date.now() + 1000 * 60) });
-		res.status(302).redirect(redirectUrl);
-	},
-
-	getByKakao: async (req, res) => {
-    const code = req.query.code; // 인가코드
-    const state = req.query.state;
-    const clientId = process.env.KAKAO_CLIENT_ID;
-    const redirectUrl =
-      state === "local"
-        ? "http://localhost:5000/kakao/callback"
-        : "http://ttot.netlify.app/payment";
-
-    const getTokenUrl = `https://kauth.kakao.com/oauth/token?grant_type=authorization_code&client_id=${clientId}&redirect_uri=${redirectUrl}&code=${code}`;
-    const getOauthDataUrl = "https://kapi.kakao.com/v2/user/me";
-
-    // 인가 코드 > 토큰 받기
-    const {
-      data: { access_token },
-    } = await axios.post(getTokenUrl);
-
-
-    // 엑세스토큰 > 데이터(이메일만 받아오면 이런 코드)
-    const {
-      data
-    } = await axios.get(getOauthDataUrl, {
-      headers: {
-        Authorization: `Bearer ${access_token}`
-      },
-    });
-
-    console.log(data);
-
-
-    const unlinkTokenUrl = `https://kapi.kakao.com/v1/user/unlink`;
-    await axios.get(unlinkTokenUrl, {
->>>>>>> 9651f58e217dd485cb3760be154e4c5e6c68a522
       headers: {
         Authorization: `Bearer ${access_token}`,
       },
     });
 
-<<<<<<< HEAD
       res.status(200).send({ data,  message: 'success' });
     } catch (err) {
       res.status(400).send({ message: err });
     }
-=======
-    // 쿠키만료시간 1분
-    res.cookie("oauthData", JSON.stringify(data), {
-      expires: new Date(Date.now() + 1000 * 60),
-    });
-
-    res.status(302).redirect("http://localhost:3000/payment");
->>>>>>> 9651f58e217dd485cb3760be154e4c5e6c68a522
   },
 };
--- route.js
+++ route.js
@@ -5,15 +5,8 @@ router.get('/test', async (req, res) => {
   res.status(200).send({ data: 'test', message: 'test' });
 });
 
-<<<<<<< HEAD
 const oauthController = require('./controllers/oauth');
 router.get('/naver/callback', oauthController.getByNaver);
 router.get('/kakao/callback', oauthController.getByKakao);
-=======
-const oauthController = require("./controllers/oauth");
-router.get("/naver/callback", oauthController.getByNaver);
-router.get("/kakao/callback", oauthController.getByKakao);
-
->>>>>>> 9651f58e217dd485cb3760be154e4c5e6c68a522
 
 module.exports = router;

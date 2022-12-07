c0cb5320edede5c0ca6b99fdac0ab2d1774670d3
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-12-07 14:22:20 +0900

    chore: pem 관련 파일 제거 및 주석제거

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const cors = require('cors');
 const express = require('express');
 const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env || 5000;
 const app = express();
-
 
 dotenv.config();
 app.use(cookieParser());
-app.use(morgan('dev')); // log
-app.use(express.json()); // req body parsing
-app.use(express.urlencoded({ extended: false })); // url query parsing
+app.use(morgan('dev')); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false })); 
 app.use(
   cors({
     origin: ['https://ttot.netlify.app', 'http://localhost:3000'],
@@ -27,7 +26,5 @@ app.listen(PORT, () => {
   console.log(`개발환경 : ${process.env.NODE_ENV}`);
   console.log(`server connect ${PORT}`);
 });
-
-
 
 module.exports = app;

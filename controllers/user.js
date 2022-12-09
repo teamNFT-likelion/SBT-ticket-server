00a77eccdc00b4a6c935b8a3c4bc0b028c5648e8
seung-ik <69374857+seung-ik@users.noreply.github.com>
2022-12-09 21:55:55 +0900

    feat: user getInfo api 추가

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,37 @@
 const axios = require('axios');
-const { getFirestore } = require('firebase-admin/firestore');
 const { User } = require('../config.js');
 
 module.exports = {
   postSignup: async (req, res) => {
     const { address, email } = req.body;
+
     if (!address || !email) {
       res.status(400).send({ msg: '이메일과 주소를 주세요' });
     }
 
-    const addressRef = User.doc(address);
-    const addressDoc = await addressRef.get();
+    const userRef = User.doc(address);
+    const userDoc = await userRef.get();
 
-    if (!addressDoc.exists) {
+    if (!userDoc.exists) {
       // db에 저장된 주소가 없는경우
       await User.doc(address).set({ address, email });
       res.status(200).send({ msg: 'signup success' });
     } else {
       // db에 이미 저장된 주소가 있는경우
-      res.status(200).send({ msg: 'already signup', data: addressDoc.data() });
+      res.status(200).send({ msg: 'already signup', data: userDoc.data() });
     }
   },
 
   getInfo: async (req, res) => {
-    res.send({ msg: 'get' });
+    const { address } = req.body;
+
+    const userRef = User.doc(address);
+    const userDoc = await userRef.get();
+
+    if (!userDoc.exists) {
+      res.status(400).send({ msg: 'no data' });
+    } else {
+      res.status(200).send({ data: userDoc.data() });
+    }
   },
 };
